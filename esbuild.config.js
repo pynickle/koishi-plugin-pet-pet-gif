e77de0ec79dfba8441ce45c211f9c625e61cf95a
pynickle <2330458484@qq.com>
2025-12-09 23:02:10 +0800

    chore: test esm

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,8 +4,8 @@ import yamlPlugin from "esbuild-plugin-yaml";
 await build({
     entryPoints: ['src/index.ts'],
     bundle: true,
-    outfile: 'lib/index.cjs',
-    format: 'cjs',
+    outfile: 'lib/index.js',
+    format: 'esm',
     platform: 'node',
     external: [
         'koishi',
